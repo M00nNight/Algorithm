f3d8b9ff50c5b7cccb9b54b7e064d7621e1057ac
Zamil Khan <106261621+M00nNight@users.noreply.github.com>
2023-10-16 16:13:53 +0530

    "linked list and Tree related stuff"

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,22 @@
-export default class SinglyLinkedList<T> {
-    public length: number;
+type Node<T> = {
+    value: T;
+    prev: Node<T>;
+    next: Node<T>;
+};
 
-    
+export default class DoublyLinkedList<T> {
+    public length: number;
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}er a URL (
 }
